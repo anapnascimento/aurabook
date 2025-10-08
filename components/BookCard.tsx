939efc87183dc1f519973070d29003cfd576a0c1
Ana Paula Souza do Nascimento <anapaula.uab@gmail.com>
2025-10-07 22:20:04 -0300

    style: Ajustes icons

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -147,7 +147,7 @@ export function ClientBookCard({ book }: BookCardProps) {
                 className="px-3 py-2 text-sm bg-red-600 text-white rounded-md hover:bg-red-700 transition-colors mb-2"
                 title="Excluir livro"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-trash-icon lucide-trash">
                   <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
                   <path d="M3 6h18" />
                   <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
--- components/header.tsx
+++ components/header.tsx
@@ -6,10 +6,17 @@ const Header = () => {
     <header className="bg-white shadow-sm border-b">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <Link href="/" className="text-xl font-bold text-purple-900">
-             Aura Books
+          <Link href="/" className="text-xl font-bold text-purple-900 flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-book-open-text-icon lucide-book-open-text mr-2 text-purple-900">
+              <path d="M12 7v14" />
+              <path d="M16 12h2" />
+              <path d="M16 8h2" />
+              <path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z" />
+              <path d="M6 12h2" />
+              <path d="M6 8h2" />
+            </svg>
+            AuraBooks
           </Link>
-
           <nav className="flex items-center space-x-4">
             <Link href="/">
               <Button variant="link">Home</Button>
